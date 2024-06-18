51efe2383666fe12110c8e60ea076a7a04a6ea0b
bakkavamshi0171 <118384205+bakkavamshi0171@users.noreply.github.com>
2024-06-18 12:09:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let trailer = document.querySelectorAll(".watch-trailer");
+
+for(let i= 0; i < 5; i++){
+    trailer[i].addEventListener("click", function(){
+        alert("Watch Trailer");
+    })
+}
